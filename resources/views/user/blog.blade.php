fa413a74171d48e6cc95c2d3cf7a413f6629ff11
AmeerMaavia <65680588+AmeerMaavia@users.noreply.github.com>
2023-01-27 23:55:09 +0500

    template page link

--- resources/views/user/blog.blade.php
+++ resources/views/user/blog.blade.php
@@ -74,7 +74,7 @@
                                                 <a href="{!!asset('user/aboutus')!!}"><span>ABOUT</span> </a>
                                             </li>
                                             <li id="3">
-                                                <a href="{!!asset('user/')!!}"><span>CREATE A MEMORIAL</span> </a>
+                                                <a href="{!!asset('admin/template/template1')!!}"><span>CREATE A MEMORIAL</span> </a>
                                             </li>
                                             <li id="4">
                                                 <a href="{!!asset('user/')!!}"><span>PLANS & FEATURES</span> </a>
@@ -158,8 +158,8 @@
                                         <li id="2">
                                             <a href="{!!asset('user/aboutus')!!}"><span>ABOUT</span> </a>
                                         </li>
-                                        <li id="3">
-                                            <a href="{!!asset('user/')!!}"><span>CREATE A MEMORIAL</span> </a>
+                                        <li id="3">                                            
+                                            <a href="{!!asset('admin/template/template1')!!}"><span>CREATE A MEMORIAL</span> </a>
                                         </li>
                                         <li id="4">
                                             <a href="{!!asset('user/')!!}"><span>PLANS & FEATURES</span> </a>
