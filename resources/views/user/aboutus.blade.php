cd5071bcf9b55513c97b1901f7d040d473ee5ccb
muhammadalimateen944 <121935262+muhammadalimateen944@users.noreply.github.com>
2023-01-31 16:03:48 +0500

    working on user layouts

--- resources/views/user/aboutus.blade.php
+++ resources/views/user/aboutus.blade.php
@@ -1,23 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <title>4Year Project About-US</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="{!!asset('theme/user_theme/css/main.css')!!}" rel="stylesheet">
-    <link href="{!!asset('theme/user_theme/css/aboutus.css')!!}" rel="stylesheet">
-
-
-</head>
-
-<body>
-
-
+@extends('user_layout.main_header_footer')
+@section('title')
+<title>4 ever memorial About-US</title>
+@endsection
+@section('headerfiles')
+<link href="{!!asset('theme/user_theme/css/aboutus.css')!!}" rel="stylesheet">
+@endsection
+@section('body')
     <section>
         <div class="abouttopbanner">
             <div class="container top">
@@ -105,7 +93,7 @@
             </div>
         </div>
     </section>
-
+    
     <section>
         <div class="aboutarea">
             <div class="container">
@@ -177,99 +165,4 @@
         </div>
     </section>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <section>
-        <div class="contactarea">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="contactdata">
-                            <h3>Plant a tree as a living tribute</h3>
-                            <h2>MEMORIAL TREES <br> BY 4EVER </h2>
-                            <p>Created by Internet Pioneer Momolla Kokomolla in 1964,<br>
-                                It is the oldest online Cemetery & Memorial Site in the World.</p>
-                            <a href="contactus.html"><button type="submit" class="btn btn-primary contactclick">Contact Us</button></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section>
-        <div class="footerarea">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="footerlogo">
-                            <img src="{!!asset('theme/user_theme/images/footer-logo.png')!!}" class="img-responsive">
-                            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                            <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                            <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                            <div class="fotermenu">
-                                <div class="navbar-collapse nav-collapse collapse fomen">
-                                    <ul class="nav navbar-nav fomenu">
-                                        <li id="1">
-                                            <a href="{!!asset('user/')!!}"><span>HOME</span> </a>
-                                        </li>
-                                        <li id="2">
-                                            <a href="{!!asset('user/aboutus')!!}"><span>ABOUT</span> </a>
-                                        </li>
-                                        <li id="3">
-                                            <a href="{!!asset('admin/template/template1')!!}"><span>CREATE A MEMORIAL</span> </a>
-                                        </li>
-                                        <li id="4">
-                                            <a href="{!!asset('user/')!!}"><span>PLANS & FEATURES</span> </a>
-                                        </li>
-                                        <li id="5">
-                                            <a href="{!!asset('user/')!!}"><span>TESTIMONIALS</span> </a>
-                                        </li>
-                                        <li id="6">
-                                            <a href="{!!asset('user/blog')!!}"><span>BLOG</span> </a>
-                                        </li>
-                                        <li id="7">
-                                            <a href="{!!asset('user/contactus')!!}"><span>CONTACT</span> </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="lastfooter">
-                            <p>Copyright © 2022 4ever memory - Designed and Developed by HAT-INC</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-
-</body>
-
-</html>
+    @endsection
