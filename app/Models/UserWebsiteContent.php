0c6e703b90ccfe7ce4009a9691b0ecd7cab6c0dc
AbubakarMian <65680588+AmeerMaavia@users.noreply.github.com>
2023-02-09 14:36:52 +0500

    working on templates

--- app/Models/UserWebsiteContent.php
+++ app/Models/UserWebsiteContent.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class UserWebsiteContent extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    protected $table='user_website_content';
 }
