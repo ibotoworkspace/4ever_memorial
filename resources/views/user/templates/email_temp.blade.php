4212f4460bb9681a34f525890231446cefc21865
AmeerMaavia <65680588+AmeerMaavia@users.noreply.github.com>
2023-08-29 16:13:20 +0500

    set email2

--- resources/views/user/templates/email_temp.blade.php
+++ resources/views/user/templates/email_temp.blade.php
@@ -1,4 +1,3 @@
-{{-- {!!dd($details[$deceased_data->b_city])!!} --}}
 <!DOCTYPE html>
 <html>
 
@@ -20,13 +19,12 @@
       padding: 20px;
       border: 1px solid #ddd;
       border-radius: 10px;
-      /* background-image: url("../"); */
       background: url(../../images/headerimage.jpg) no-repeat;
-            background-size: cover;
+      background-size: cover;
     }
 
     .header {
-        background: url(../../images/headerimage.jpg) no-repeat;
+      background: url(../../images/headerimage.jpg) no-repeat;
       background-size: cover;
       color: #fff;
       padding: 20px;
@@ -58,7 +56,6 @@
       font-size: 18px;
       transition: background-color 0.3s ease;
       margin: 20px 0px;
-
     }
 
     .memorial-link:hover {
@@ -72,30 +69,19 @@
     <div class="header">
       <h1>In Loving Memory</h1>
     </div>
-    <?php 
-       $gender = {!!$details[$deceased_data->gender]!!};
-       if ($gender == 'male') {
-        $gender_helping_verb = 'he';
-        
-       }
-       else {
-        $gender_helping_verb = 'she';
-       }
-    ?>
     <div class="invitation">
-      <p>Dear {!!$details['to']!!},</p>
-      <p>We invite you to join us in celebrating the life and cherished memories of our beloved {!!$details[$deceased_data->f_name]!!}, who passed away
-        on {!!$details[$deceased_data->p_year]!!}.</p>
+      <p>Dear {{ $details['to'] }},</p>
+      <p>We invite you to join us in celebrating the life and cherished memories of our beloved {{ $details['deceased_data']['f_name'] }}, who passed away
+        on {{ $details['deceased_data']['p_year'] }}.</p>
       <p>We have created a memorial website where friends and family can come together to share stories, photos, and
-        memories. It is a space to honor {!!$details[$deceased_data->f_name]!!}'s life and the impact {!!$gender_helping_verb!!} had on all of us.</p>
+        memories. It is a space to honor {{ $details['deceased_data']['f_name'] }}'s life and the impact {{ $details['deceased_data']['gender'] == 'male' ? 'he' : 'she' }} had on all of us.</p>
       <p>Please visit the memorial website using the link below:</p>
       <p style="text-align: center;">
-        <a href="https://stagging.hatinco.com//public/user/get_memorial/{!!$details[$deceased_data->email]!!}" class="memorial-link">Visit Memorial Website</a>
+        <a href="https://stagging.hatinco.com/public/user/get_memorial/{{ $details['deceased_data']['email'] }}" class="memorial-link">Visit Memorial Website</a>
       </p>
-      <p>{!!$details[$deceased_data->f_name]!!}'s memory lives on through the stories we share, and we hope you can contribute to this tribute.</p>
+      <p>{{ $details['deceased_data']['f_name'] }}'s memory lives on through the stories we share, and we hope you can contribute to this tribute.</p>
       <p style="text-align: center;">With warmest regards,</p>
-      {{-- <p style="text-align: center;">[Your Name]</p> --}}
-      <p style="text-align: center;">{!!$details[$deceased_data->relation]!!}</p>
+      <p style="text-align: center;">{{ $details['deceased_data']['relation'] }}</p>
     </div>
   </div>
 </body>
