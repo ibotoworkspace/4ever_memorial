57d98c3d670ec3bf12b437672396d9a23ca2a0a3
AmeerMaavia <65680588+AmeerMaavia@users.noreply.github.com>
2023-09-08 16:02:13 +0500

    contact page updated

--- resources/views/user/contactus.blade.php
+++ resources/views/user/contactus.blade.php
@@ -1,100 +1,111 @@
+@extends('user_layout.main_header_footer')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>new Password</title>
-    <style>
-        /* Add your custom email styles here */
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
-        }
-        .container {
-            max-width: 600px;
-            margin: 0 auto;
-            padding: 20px;
-            background-color: #ffffff;
-            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-        }
-        .header {
-            background-color: #007bff;
-            text-align: center;
-            padding: 20px 0;
-        }
-        .header img {
-            max-width: 100px;
-        }
-        h1 {
-            color: #333;
-        }
-        p {
-            color: #555;
-        }
-        .password {
-            font-weight: bold;
-            font-size: 18px;
-            background-color: #f4f4f4;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            overflow-wrap: break-word; /* Ensure long passwords wrap */
-        }
-        a {
-            color: #007bff;
-            text-decoration: none;
-        }
-        .copy-button {
-            display: inline-block;
-            padding: 10px 20px;
-            background-color: #007bff;
-            color: #fff;
-            text-decoration: none;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-    </style>
-</head>
-<body>
-    <div class="container">
-        <div class="header">
-            <img src="../images/logo.png" alt="Company Logo">
+@section('title')
+    <title>4Year Project Contact-US</title>
+@endsection
+
+
+@section('headerfiles')
+    <link href="{!! asset('theme/user_theme/css/contactus.css') !!}" rel="stylesheet">
+@endsection
+
+@section('body')
+    <section>
+        <div class="contacttopbanner">
+            @include('user_layout.components.banner_menu')
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="bannerdata aboutheading">
+                            <h1>CONTACT</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <h1>New Password</h1>
-        <p>Hello,</p>
-        <p>You've requested a new password for your account. Here is your new password:</p>
-        <p class="password" id="new-password">'new_password'</p>
-        <button class="copy-button" onclick="copynewPassword()">Copy Password</button>
-        <p>Please log in using this new password.</p>
-        <p>If you didn't request this new password, please contact us immediately.</p>
-        <p>Thank you,</p>
-        <p>Reels Pro Club</p>
-        <p>Contact us at <a href="mailto:info@reelsproclub.com">info@reelsproclub.com</a></p>
-    </div>
+    </section>
 
-    <script>
-        function copynewPassword() {
-            var newPassword = document.getElementById('new-password');
-            var tempPasswordText = newPassword.innerText;
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="contactdata conuniq">
+                        <div class="infobox">
+                            <div class="infoboximg">
+                                <img src="{!! asset('theme/user_theme/images/email.png') !!}" class="img-responsive">
+                            </div>
+                            <div class="infoboxtext">
+                                <p>info@4evermemorial.com</p>
 
-            // Create a new textarea element to copy the text
-            var tempTextarea = document.createElement('textarea');
-            tempTextarea.value = tempPasswordText;
-            document.body.appendChild(tempTextarea);
+                            </div>
+                        </div>
+                        <div class="infobox">
+                            <div class="infoboximg">
+                                <img src="{!! asset('theme/user_theme/images/phone.png') !!}" class="img-responsive">
+                            </div>
+                            <div class="infoboxtext">
+                                <p>+1909 559 9031</p>
 
-            // Select and copy the text
-            tempTextarea.select();
-            document.execCommand('copy');
+                            </div>
+                        </div>
+                        <div class="infobox">
+                            <div class="infoboximg loca">
+                                <img src="{!! asset('theme/user_theme/images/location.png') !!}" class="img-responsive">
+                            </div>
+                            <div class="infoboxtext">
+                                <p>4Ever Memorial
+                                    6101 Cherry Avenue Suite 102A Fontana,
+                                    CA 92336 USA
+                                </p>
 
-            // Remove the new textarea
-            document.body.removeChild(tempTextarea);
-
-            // Optionally, provide feedback to the user
-            alert('new password copied to clipboard!');
-        }
-    </script>
-</body>
-</html>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-8">
+                    <div class="contactform">
+                        <h2>Get In Touch With Us</h2>
+                        <form>
+                            <!-- <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Type of inquiry*</label>
+                                    <select class="form-control" id="exampleFormControlSelect1">
+                                        <option>Make a Selection...</option>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div> -->
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Enter Name*</label>
+                                <input type="text" class="form-control" id="text" aria-describedby="text"
+                                    placeholder="Enter Name">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email address*</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp" placeholder="Enter email">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Memorial Name*</label>
+                                <input type="text" class="form-control" id="text" aria-describedby="text"
+                                    placeholder="(if applies)">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Your message*</label>
+                                <input type="text" class="form-control" id="text" aria-describedby="text"
+                                    placeholder="Please add your subject here">
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"
+                                    placeholder="Please enter the specific details of your request. Please provide as much information as possible so we can help you quickly"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary contactclick">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
